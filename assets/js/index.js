2090d6e60f550183dda3a6e74abf3a04289d157b
sushmita12345 <38626023+sushmita12345@users.noreply.github.com>
2022-02-14 14:36:15 +0530

    Revert "F11 textutility"

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-const getStartBtn = document.querySelector('#comp-lib-btn');
-
-
-getStartBtn.addEventListener('click', clickStart);
-
-function clickStart() {
-    location.href = "/components/docs.html";
-}
